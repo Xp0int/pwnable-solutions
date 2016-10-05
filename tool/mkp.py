e92ecd21dbd0c4d45b8fab56cfc2dcb6da5f1425
Seas0n <306443228@qq.com>
2016-10-05 15:24:54 +0800

    modify layout of repo

--- tool/mkp.py
+++ tool/mkp.py
@@ -8,31 +8,43 @@ GREEN   = COLORED(RAW, 'green')
 CYAN    = COLORED(RAW, 'cyan')
 
 # extension for zio
+# write
 def w(self, s): 
     self.write(s)
 
+# writeline
 def wl(self, s = ''): 
     if isinstance(s, (int, long)):
         self.writeline(str(s)) 
     else: 
         self.writeline(s)
 
+# writelines
 def ws(self ,sequence): 
     self.writelines( [str(i) if isinstance(i, (int, long)) else i for i in sequence] )
 
+# read
 def r(self, size = None, timeout = 1):
     return self.read(size, timeout)
 
+# readline
 def rl(self, size = 1):
     return self.read_line(size)
 
+# read_until
 def ru(self, pattern_list, timeout = 1, searchwindowsize = None):
     return self.read_until(pattern_list, timeout, searchwindowsize)
 
-def wa(self, pattern_list, s, timeout = 1, searchwindowsize = None):
+def wa(self, pattern_list, s, timeout = -1, searchwindowsize = None):
+    self.read_until(pattern_list, timeout, searchwindowsize)
+    self.writeline(s)
+
+# writeline_after
+def wla(self, pattern_list, s, timeout = 1, searchwindowsize = None):
     self.read_until(pattern_list, timeout, searchwindowsize) 
     self.writeline(s)
 
+# wrtelins_after
 def wsa(self, pattern_list, sequence, timeout = 1, searchwindowsize = None):
     self.read_until(pattern_list, timeout, searchwindowsize) 
     self.writelines( [str(i) if isinstance(i, (int, long)) else i for i in sequence] )
@@ -45,6 +57,7 @@ setattr(zio, 'w', w)
 setattr(zio, 'wl', wl)
 setattr(zio, 'ws', ws)
 setattr(zio, 'wa', wa)
+setattr(zio, 'wla', wa)
 setattr(zio, 'wsa', wsa)
 setattr(zio, 'r', r)
 setattr(zio, 'rl', rl)

